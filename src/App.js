2c621d78effea3d92e0a244c6a16d356e30f2586
Adnan Abrar <83346033+adnanabrar786@users.noreply.github.com>
2022-02-01 12:10:00 -0800

    COMMIT

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FacebookLoginC from './componenet/FacebooLoginC';
+
+function App() {
+  return (
+    <>
+      react app
+       <FacebookLoginC/>
+    </>
+  );
+}
+
+export default App;
